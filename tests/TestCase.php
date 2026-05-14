e19a809d9e3971312371441c3d75b48522a242ac
PedroSL17 <pedrosanchezlopez17@gmail.com>
2026-05-14 20:50:17 +0200

    test fix

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,16 +6,41 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    public function createApplication()
+    {
+        $this->prepareCompiledViewsPath();
+
+        $app = parent::createApplication();
+
+        $app['config']->set('view.compiled', $this->compiledViewsPath());
+
+        return $app;
+    }
+
     protected function setUp(): void
     {
         parent::setUp();
 
-        $compiledViewsPath = sys_get_temp_dir().'/easybooking-testing-views';
+        $this->prepareCompiledViewsPath();
+
+        config(['view.compiled' => $this->compiledViewsPath()]);
+    }
+
+    private function prepareCompiledViewsPath(): void
+    {
+        $compiledViewsPath = $this->compiledViewsPath();
 
         if (! is_dir($compiledViewsPath)) {
             mkdir($compiledViewsPath, 0775, true);
         }
 
-        config(['view.compiled' => $compiledViewsPath]);
+        putenv('VIEW_COMPILED_PATH='.$compiledViewsPath);
+        $_ENV['VIEW_COMPILED_PATH'] = $compiledViewsPath;
+        $_SERVER['VIEW_COMPILED_PATH'] = $compiledViewsPath;
+    }
+
+    private function compiledViewsPath(): string
+    {
+        return sys_get_temp_dir().'/easybooking-testing-views-'.substr(md5((string) realpath(__DIR__.'/..')), 0, 12);
     }
 }
